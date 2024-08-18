491166697d42011550cc614427ecef17b8efa591
Joshua Charles <josh@asleepingturtle.com>
2024-08-17 22:41:04 -0400

    Updated to use postgres

--- mysite/mysite/settings/local.py
+++ mysite/mysite/settings/local.py
@@ -4,7 +4,10 @@ DEBUG = True
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('DB_NAME'),
+        "USER": config('DB_USER'),
+        "PASSWORD": config('DB_PASSWORD'),
+        "HOST": config('DB_HOST'),
     }
 }
